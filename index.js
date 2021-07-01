400ead26d54c4003adc75e2e10153352fe639976
Starpelly <braedonwilcox@gmail.com>
2021-06-30 21:33:36 -0400

    really nigga

--- index.js
+++ index.js
@@ -42,9 +42,4 @@ bot.on("message", async message => {
 
 })
 
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});
-
 bot.login(process.env.token);
