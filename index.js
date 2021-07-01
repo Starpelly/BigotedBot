707d6b3b0516936ea54ab1f0df78c1c422a853b6
Starpelly <braedonwilcox@gmail.com>
2021-06-30 21:31:14 -0400

    aaa

--- index.js
+++ index.js
@@ -42,4 +42,9 @@ bot.on("message", async message => {
 
 })
 
-bot.login(botsettings.token);
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
+
+bot.login(process.env.token);
