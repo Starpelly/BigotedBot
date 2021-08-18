1657e0c97d91b9d049d79ea55170770d599ad657
Starpelly <braedonwilcox@gmail.com>
2021-08-17 21:58:12 -0400

    walroose custom images + rule34 nsfw channels only

--- commands/r34.js
+++ commands/r34.js
@@ -4,47 +4,53 @@ const botconfig = require("../botsettings.json");
 const r34api = new (require('r34api.js'));
 
 module.exports.run = async (bot, message, args) => {
-            const formats = ["jpg", 'png', 'gif'];
-            const random = Math.floor(Math.random() * formats.length);
-            var data = await r34api.random(1, formats[random])
+    const formats = ["jpg", 'png', 'gif'];
+    const random = Math.floor(Math.random() * formats.length);
+    var data = await r34api.random(1, formats[random])
             
-            try {
+    if (message.channel.nsfw) {
+        try {
 
-                if (args == 'delete')
-                {
-                    return console.log('kinda works');
-                }
-
-                var image = data.data[0].media;
-                var tags = data.data[0].tags;
-                var url = data.data[0].post;
-                var dimension = data.data[0].dimension;
-                
-                embed = new Discord.MessageEmbed()
-                        .setTitle(`Random rule34 image`)
-                        .setURL(url)
-                        .addField( "image resolution", dimension)
-                        .setImage(image)
-                        .setDescription(tags.join())
-                        
-                return message.lineReply(embed)
-                .then(function (message) {
-                    message.react("👍")
-                    message.react("➖")
-                    message.react("👎")
-                });;
-            } catch (error) {
-                embed = new Discord.MessageEmbed()
-                        .setTitle('ERROR HANDLER')
-                        .setDescription(`something happened, message this error to Starpelly. **"${error}"**`)
-                return message.lineReply(embed);
+            if (args == 'delete')
+            {
+                return console.log('kinda works');
             }
+
+            var image = data.data[0].media;
+            var tags = data.data[0].tags;
+            var url = data.data[0].post;
+            var dimension = data.data[0].dimension;
+            
+            embed = new Discord.MessageEmbed()
+                    .setTitle(`Random rule34 image`)
+                    .setURL(url)
+                    .addField( "image resolution", dimension)
+                    .setImage(image)
+                    .setDescription(tags.join())
+                    
+            return message.lineReply(embed)
+            .then(function (message) {
+                message.react("👍")
+                message.react("➖")
+                message.react("👎")
+            });;
+        } catch (error) {
+            embed = new Discord.MessageEmbed()
+                    .setTitle('ERROR HANDLER')
+                    .setDescription(`something happened, message this error to Starpelly. **"${error}"**`)
+            return message.lineReply(embed);
+        }
+    }
+    else
+    {
+        return message.lineReply("not in nsfw channel");
+    }
 }
 
 module.exports.config = {
     name: "r34",
-    description: "generates a random slur",
-    usage: "b!slur",
+    description: "generates a rule34 image",
+    usage: "b!r34",
     accessableby: "Members",
-    aliases: []
+    aliases: ["rule34", "34", "walrooseporn64", "superepicporncommand", "lololol"]
 }
--- commands/walroose.js
+++ commands/walroose.js
@@ -2,21 +2,18 @@ const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
 
 const Canvas = require('canvas');
+const CanvasFilters = require('canvas-filters');
 
 const drawMultilineText = require('canvas-multiline-text')
 
-
 module.exports.run = async (bot, message, args) => {
 
-    const words = args.join(' ');
+    let words = args.join(' ');
+    const words2 = words.split('/');
 
     Canvas.registerFont("./fonts/Nanami-Rounded-Book.ttf", { family: 'Nami' });
     const canvas = Canvas.createCanvas(2046 / 2, 1152 / 2)
     const ctx = canvas.getContext('2d');
-
-    const background = await Canvas.loadImage('./images/walroose.png');
-    const foreground = await Canvas.loadImage('./images/1984.png');
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
     ctx.fillStyle = "white";
     function drawStrokedText(text, x, y)
@@ -32,17 +29,89 @@ module.exports.run = async (bot, message, args) => {
         ctx.restore();
     }
 
-    ctx.font = applyText(canvas, words.toUpperCase(), 'Nami');
+    let background;
 
+    let messageAttachment;
+
+    async function drawBG(customIMG)
+    {
+        if (customIMG)
+        {
+            background = await Canvas.loadImage('./images/walroosegone.png');
+        }
+        else if (customIMG == false)
+        {
+            background = await Canvas.loadImage('./images/walroose.png');
+        }
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    }
+
+    function drawFG(fg)
+    {
+        ctx.drawImage(fg, 450, -10, 576, 596);
+    }
+
+    if (message.reference)
+    {
+        var refMessage = message.reference;
+        let refer = message.channel.messages.fetch(refMessage.messageID);
+        
+        let result = await refer;
+        
+        messageAttachment = result.attachments.size > 0 ? result.attachments.array()[0].url : null
+
+        if (messageAttachment != null)
+        {
+            drawBG(true);
+            const foreground = await Canvas.loadImage(messageAttachment);
+            drawFG(foreground);
+            if (words2[1] != undefined) {
+                words = words2[1];
+            }
+            else {
+                if (result.content.length > 0)
+                    words = result.content;
+            }
+        }
+        else
+        {
+            drawBG(false);
+            background = await Canvas.loadImage('./images/walroose.png');
+            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+            words = result.content;
+        }
+    }
+    else if (words === '')
+    {
+        message.lineReply('no arguments, sending template');
+        drawBG(false);
+        background = await Canvas.loadImage('./images/walroose.png');
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    }
+    else
+    {
+        messageAttachment = message.attachments.size > 0 ? message.attachments.array()[0].url : null
+
+        if (messageAttachment == null)
+        {
+            drawBG(false);
+            background = await Canvas.loadImage('./images/walroose.png');
+            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        }
+        else
+        {
+            drawBG(true);
+            const foreground = await Canvas.loadImage(messageAttachment);
+            drawFG(foreground);
+        }
+    }
+
+    ctx.font = applyText(canvas, words.toUpperCase(), 'Nami');
     ctx.textAlign = "center";
     drawStrokedText(words.toUpperCase(), 1023 / 2, canvas.height / 1.7);
 
     const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'POLITICS.png');
-    
-    if (words === '')
-    {
-        message.lineReply('no arguments, sending template');
-    }
+
     message.lineReply(attachment);
     console.log(words)
 }
